6ce418db527ddb1287a28ff5156bbeb3a51749b7
Ignacio Babbini <ignababbini@gmail.com>
2017-10-08 23:23:12 -0300

    added first draft for social login

---
+++ DungeonMasterManager/app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+  before_action :authenticate
+
+  def show
+  end
+end
---
+++ DungeonMasterManager/config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, "100231593184-bde6ppfdieffrpi4n6qsqrug4on6k2mo.apps.googleusercontent.com", "LlUDfips46a6U7oub_wkrE53"
+end
